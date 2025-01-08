90342bb0b179852e5f55c7c7f02df1eb4bf289bf
Chitralekhayaduvanshi <102354726+Chitralekhayaduvanshi@users.noreply.github.com>
2025-01-08 17:26:59 +0530

    build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+
+import animate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -33,5 +35,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } 
